98b7941eee86a7a6b1d0bc3a2920b41495ddad3b
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-03-03 20:09:32 +0300

    Done building it

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+
 function Navbar() {
   return (
-    <div>
+    <nav>
        <Link href="/">Home</Link>
        <Link href="/about">About</Link>
        <Link href="/posts">Posts</Link>
-    </div>
+    </nav>
   )
 }
 
